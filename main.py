ec3750e77e8f83a372e15de866660e44cb33bc24
Sarah Morrison <sarahmorrison@MacBook-Pro.local>
2024-09-04 16:34:44 -0700

    initial

---
+++ main.py
@@ -0,0 +1,23 @@
+import math
+#This is a one line comment
+
+"""
+This is a multiline comment
+AKA a block comment
+"""
+
+print("hello world")
+
+#Variables
+# A varaible stores a value and has a name that is convienent
+x = 5 # read this as "x is assigned 5"
+#not "x equals 5"
+print(x)
+print(type(x))
+#now we are overvwriting x with a new value
+x = 5.7
+print (x)
+print(type(x))
+#float data type
+print(math.pow(2,3))
+print(math.asin(0))
